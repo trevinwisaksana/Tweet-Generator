8a1fa94d6d5895846c3205fdd2f48a479ab04492
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-11-08 21:12:36 -0800

    3rd Challenge

---
+++ word_frequency.py
@@ -0,0 +1,28 @@
+"""
+A histogram() function which takes a source_text argument
+(can be either a filename or the contents of the file as a string, your choice)
+and return a histogram data structure that stores each unique word along with
+the number of times the word appears in the source text.
+
+A unique_words() function that takes a histogram argument and returns the total
+count of unique words in the histogram. For example, when given the histogram
+for The Adventures of Sherlock Holmes, it returns the integer 8475.
+
+A frequency() function that takes a word and histogram argument and returns the
+number of times that word appears in a text. For example, when given the word
+"mystery" and the Holmes histogram, it will return the integer 20.
+"""
+
+# Opening the source text which is Beawulf.txt
+source_text = open("Beawulf.txt", "r")
+
+
+# Histogram Functions
+def histogram(text):
+    # Write code
+    testing
+
+# Unique Words Function
+
+
+# Frequency Function
