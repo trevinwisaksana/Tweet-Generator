1d6a55a50f11c488ba9b8342a01a783df0979584
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-11-09 10:31:01 -0800

    Not complete

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+
+
 """
 A histogram() function which takes a source_text argument
 (can be either a filename or the contents of the file as a string, your choice)
@@ -14,15 +16,60 @@ number of times that word appears in a text. For example, when given the word
 """
 
 # Opening the source text which is Beawulf.txt
-source_text = open("Beawulf.txt", "r")
+source_text = open("Horatius.txt", "r")
+
+
+# Contains a list of repeated words
+repeated_words_list = []
+# repeated_words_list = [[] for i in range(0, len(entire_text))]
+
+
+# An function that stores the word in the repeated_words_list
+def repeated_words_counter(text):
+    # For loop through each object in array.
+    for i in text:
+        # If the word doesn't match
+        if i not in repeated_words_list:
+            # If it doesn't match, create a new array
+            repeated_words_list.append([i.lower()])
+            print("New array")
+        # If the word already exists in the array.
+        if i.lower() in repeated_words_list:
+            # The index value of the word in the repeated_words_list array
+            word_index = repeated_words_list.index(i.lower())
+            # Append to the nested word array in the repeated_words_list
+            repeated_words_list[word_index].append(i.lower())
+            print("Appended again")
+
+
+'''
+SUDO CODE:
+# Loop through the words that are in the entire_text
+# If there are words that matches, add it to an existing array
+  with the same word
+# If the it doesn't match with any other word, create a new array
+# Count the number of words within each nested array to get the frequency and
+  unique words
+# If the words are used less than 5 times, they are unique
+# If not, they are are commonly used words
+'''
 
 
 # Histogram Functions
 def histogram(text):
-    # Write code
-    testing
+    # Reading and splitting the words to be selectable.
+    entire_text = text.read().split()
+    # The repeated_words_counter to find similar words.
+    repeated_words_counter(entire_text)
+
 
 # Unique Words Function
 
 
 # Frequency Function
+
+if __name__ == "__main__":
+    testing = histogram(source_text)
+    # print(testing)
+    list = repeated_words_list
+    print(list)
