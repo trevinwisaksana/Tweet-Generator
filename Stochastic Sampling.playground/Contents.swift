b205f6b430f1a7be451f761295b371df8979018f
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-11-15 16:38:59 -0800

    First refactoring
    Renamed functions and reworded comments

--- Stochastic Sampling.playground/Contents.swift
+++ Stochastic Sampling.playground/Contents.swift
@@ -2,35 +2,42 @@
 
 import UIKit
 
-// MARK: - Accessing Corpus
-// Get the file path for the file "Horatius.txt" in the Playground bundle
-let filePath = Bundle.main.path(forResource: "Horatius", ofType: "txt")
-
-// Get the contentData
-let contentData = FileManager.default.contents(atPath: filePath!)
-
-// Get the string
-let content = NSString(data: contentData!, encoding: String.Encoding.utf8.rawValue) as? String
-
 // MARK: - Creating Distribution
-func dictionaryHistogram(sourceText: String) -> [String]  {
+func dictionaryHistogram(sourceText: String) -> [String : Int]  {
     // Entire text that's splitted into key value pairs
-    let entireSplittedText = sourceText.components(separatedBy: ", ")
-    // Word dictionary 
-    var wordDictionary: [String : Int] = [:]
-    // Loops through each word in the entire text 
+    let entireSplittedText = sourceText.components(separatedBy: " ")
+    // Word dictionary
+    var wordDictionary = [String : Int]()
+    // Loops through each word in the entire text
     for word in entireSplittedText {
+        // keyValue is the frequency value
+        var keyValue = 0
         // Going to every word and makes the Value 1 because it's the first word there.
         if wordDictionary.keys.contains(word) {
             // Making all words lowercased
-            wordDictionary[word] += 1
+            keyValue = wordDictionary[word]! + 1
+            wordDictionary.updateValue(keyValue, forKey: word)
         } else {
             wordDictionary[word] = 1
         }
     }
     
-    return entireSplittedText
+    return wordDictionary
 }
+
+// MARK: - Accessing Corpus
+// Get the file path for the file "Fish.txt" in the Playground bundle
+if let filePath = Bundle.main.path(forResource: "Fish", ofType: "txt") {
+    // Get the contentData
+    let contentData = FileManager.default.contents(atPath: filePath)
+    // Get the string
+    let content = NSString(data: contentData!, encoding: String.Encoding.utf8.rawValue) as? String
+    // Using the dictionaryHistogram function to process the array into a histogram
+    dictionaryHistogram(sourceText: content!)
+}
+
+
+
 
 
 
